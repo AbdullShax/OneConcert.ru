12f68a8e09a06194bc84ae746e23101b7ddbc870
AbdullShax <shaxriyor9797@mail.ru>
2023-05-21 18:32:10 +0500

    media_start

--- js/app.js
+++ js/app.js
@@ -1,9 +1,17 @@
-var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 3,
-  spaceBetween: 30,
-  freeMode: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-});
+
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:10,
+  nav:true,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:3
+      },
+      1000:{
+          items:5
+      }
+  }
+})
